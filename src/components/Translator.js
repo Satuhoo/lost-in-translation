c18d2312bb4d2bed1042acd57a2e9a1396ab4209
satu.heikkonen <satu.heikkonen@5CG05206R0.nordic.local>
2021-03-04 14:37:06 +0200

    feat: add sign images and functionality for Translator component

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 function Translator() {
+    const [inputWord, setInputWord] = useState("");
+    const [translation, setTranslation] = useState([]);
+
+    const handleInputChange = (event) => {
+        setInputWord(event.target.value);
+    }
+
+    const translate = (event) => {
+        event.preventDefault();
+        let characters = inputWord.toLowerCase().split("");
+
+        let signs = [];
+        for (let char in characters) {
+            signs.push(require(`../assets/individial_signs/${characters[char]}.png`).default)
+        }
+        setTranslation(signs);
+    }
+
     return (
         <div>
-            <h1>Translator</h1>
+            <form onSubmit={translate}>
+                <input value={inputWord} onChange={handleInputChange} placeholder="Type what you want to translate"></input>
+                <button type="submit">Translate</button>
+            </form>
+            <div>
+            {translation.map((sign, index) => <img key={index} src={sign} alt="img" />)}
+            </div>
         </div>
     )
 }
