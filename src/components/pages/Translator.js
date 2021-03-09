0ffe1578a4b2185fa28e92590bf43df864325fff
satu.heikkonen <satu.heikkonen@5CG05206R0.nordic.local>
2021-03-09 09:17:22 +0200

    feat: adds method which checks special characters from the input word and shows info message in the translation page

--- src/components/pages/Translator.js
+++ src/components/pages/Translator.js
@@ -8,6 +8,8 @@ import '../styles/Translator.css';
 function Translator() {
     const [inputWord, setInputWord] = useState("");
     const [translation, setTranslation] = useState([]);
+    const [translatedWord, setTranslatedWord] = useState("");
+    const [showInfoMessage, setShowInfoMessage] = useState(false);
 
     const handleInputChange = (event) => {
         setInputWord(event.target.value);
@@ -16,12 +18,25 @@ function Translator() {
     //Checks if the input word is not empty string, saves it to storage and calls translate method
     const handleTranslate = (event) => {
         event.preventDefault();
+        setShowInfoMessage(false);
         if (inputWord !== "") {
             addToStorage(inputWord);
             let signs = translate(inputWord);
             setTranslation(signs);
+            setTranslatedWord(inputWord);
         }
+        checkSpecialCharacters(inputWord)
         setInputWord("");
+    }
+
+    //Checks if there is special characters or numbers in the input word
+    const checkSpecialCharacters = (inputWord) => {
+        //Replaces all whitespaces so they are not counting for special characters
+        let word = inputWord.replaceAll(" ", "");
+        if (word.match("[^A-Za-z]")) {
+            //Sets infomessage to shown if there is something else than regular letters in the input word
+            setShowInfoMessage(true); 
+        }
     }
 
     return (
@@ -30,8 +45,12 @@ function Translator() {
             <Form value={inputWord} handleChangeValue={handleInputChange} placeholder="Type what you want to translate"
                 submit={handleTranslate} path="#"/>
             </div>
+            {translation.length > 0 && <p className="translated-word">Translated: {translatedWord}</p>}
+            {showInfoMessage && <p className="info-msg">
+                NB! You can't translate numbers or special characters. All letters in entered word/sentence have translated
+                </p>}
             <div className="translator-box-container">
-                <Translation translation={translation}/>
+                <Translation translation={translation} translatedWord={translatedWord}/>
             </div>
         </div>
     )
